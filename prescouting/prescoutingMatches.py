fa52e241a78941c67186327027d51a65c1d4afc6
dmatlin16 <dmatlin@ucvts.tec.nj.us>
2018-03-12 01:41:59 -0400

    added small features and made minor bugfixes
    more commenting to be done later
    
    Co-Authored-By: michaelmat72 <michaelmat72@users.noreply.github.com>

--- prescouting/prescoutingMatches.py
+++ prescouting/prescoutingMatches.py
@@ -1,17 +1,16 @@
-"""Given an event key, this program returns matches to prescout for each team attending that event as a dict.
-   Format: {1257: [match1, match2, match3, match4], 7521: [match1, match2, match3, match4], ...} or
-           {1257: [match2, match2], 7521: [match1, match2], ...}"""
+"""Given proper input, this program returns matches to prescout for each team based on mode of input.
+   Format: comma-separated table with columns team,event,matches"""
 
-import json, requests
+import json, requests, csv
 from datetime import date
 year = date.today().year
 
 # Create session for all TBA requests
-s = requests.Session()
+session = requests.Session()
 # Try to read authKey from tbaAuth.txt
 try:
     authKey = open("tbaAuth.txt", "r")
-    s.headers.update({'X-TBA-Auth-Key': authKey.read().strip()})
+    session.headers.update({'X-TBA-Auth-Key': authKey.read().strip()})
     authKey.close()
 # Exit program if tbaAuth.txt does not exist
 except FileNotFoundError:
@@ -20,16 +19,29 @@ except FileNotFoundError:
 
 # Makes a TBA request, returns content of request as json if possible, returns 1 if there's an error
 def tba_request(query):
-    try:
-        return json.loads(s.get("http://www.thebluealliance.com/api/v3/" + query).text)
-    except:
-        return 1
+    while True:
+        try:
+            return json.loads(session.get(f"http://www.thebluealliance.com/api/v3/{query}", timeout = 60).text)
+        except requests.exceptions.ConnectionError:
+            print("Connection error: check your network settings. ", end = "")
+        except requests.exceptions.Timeout:
+            print("API request timeout. ", end = "")
+        retry = input("Retry request? Y/N: ").lower().strip()
+        if retry  == "y" or retry == "yes":
+            pass
+        elif retry == "n" or retry == "no":
+            print("Exiting program.")
+            exit()
+        else:
+            print("Error: input invalid. Try again.")
 
-mode = ""
+if "frc1" not in tba_request("teams/0/keys"):
+    print("Error: TBA API authorization key invalid. Please use a valid auth key and try again.")
+    exit()
 
-raw_districts = tba_request("districts/" + str(year))
+raw_districts = tba_request(f"districts/{year}")
 district_key_list = [district["abbreviation"] for district in raw_districts]
-event_codes = tba_request("events/" + str(year) + "/keys")
+event_codes = tba_request(f"events/{year}/keys")
 
 # If event is in the future, or if event type is undefined, offseason, or preseason, return that it is not suitable for scouting
 def is_scoutable(event_type, event_date):
@@ -39,20 +51,22 @@ def tba_date_to_obj(string):
     return date(int(string[:4]), int(string[5:7]), int(string[8:10]))
 
 def is_team(team):
-    info = tba_request("team/frc" + team + "/simple")
+    info = tba_request(f"team/frc{team}/simple")
     if "key" in info:
         return True
 
+mode = ""
+
 while True:
-    print("Enter\n1. an event key (only letters, no year) to select a specific event,\n2. a district name to get a list of all upcoming events for the district, or\n3. a comma-separated list of team numbers for which you want matches.")
+    print("Enter\n1. an event key to select a specific event,\n2. a district name to get a list of all upcoming events for the district, or\n3. a comma-separated list of team numbers for which you want matches.")
     key_input = input().lower().strip()
     if key_input in district_key_list:
         mode = "district"
         break
-    elif key_input.isdigit() or "," in key_input:
+    elif key_input.isdigit() or "," in key_input and key_input != ",":
         mode = "csv"
         break
-    elif (str(year) + key_input) in event_codes or key_input in event_codes:
+    elif f"{year}{key_input}" in event_codes or key_input in event_codes:
         mode = "code"
         break
     else:
@@ -60,8 +74,8 @@ while True:
 
 # Makes sure that the district key entered is valid
 if mode == "district":
-    print("\n" + key_input.upper() + " events:")
-    district_events = tba_request("district/" + str(year) + key_input + "/events")
+    print(f"\n{key_input.upper()} events:")
+    district_events = tba_request(f"district/{year}{key_input}/events")
     district_events = sorted(district_events, key = lambda x: tba_date_to_obj(x["start_date"]))
     for event_item in tuple(enumerate(district_events)):
         print(f"{event_item[0] + 1}: {event_item[1]['short_name']}")
@@ -79,13 +93,13 @@ if mode == "district":
 
 if mode == "code":
     if key_input not in event_codes:
-        event_key = str(year) + key_input
+        event_key = f"{year}{key_input}"
     else:
         event_key = key_input
 
 if mode == "district" or mode == "code":
     # Makes a list teams with all teams attending the specified event
-    event_teams = tba_request("event/" + event_key + "/teams")
+    event_teams = tba_request(f"event/{event_key}/teams")
     teams = []
     for team in event_teams:
         teams.append(team["team_number"])
@@ -125,13 +139,24 @@ while True:
     else:
         print("\nError: matches with video must be Y/N. Try again: ", end = "")
 
+print("\nOutput teams without scoutable events? Y/N: ", end = "")
+while True:
+    eventless = input().lower().strip()
+    if eventless == "y" or eventless == "yes":
+        eventless = False
+        break
+    elif eventless == "n" or eventless == "no":
+        eventless = True
+        break
+    else:
+        print("\nError: Output of teams without events must be Y/N. Try again: ", end = "")
+
 # Initialize scouting_events variable, which holds the team number and event key of latest scoutable event
 scouting_events = {}
 # For each team, add its latest scoutable event to scouting_events
 for team in teams:
-    team_events = tba_request("team/frc" + str(team) + "/events/" + str(year) + "/simple") # Retrieves list of team's events for season
+    team_events = tba_request(f"team/frc{team}/events/{year}/simple") # Retrieves list of team's events for season
     team_events = sorted(team_events, key = lambda x: tba_date_to_obj(x["start_date"])) # Sorts by start date
-    event_index = 0
     for event in reversed(team_events): # For each potential event (going by most recent), use that event if it's scoutable 
         if is_scoutable(event["event_type"], tba_date_to_obj(event["start_date"])):
             scouting_events[team] = event["key"]
@@ -144,10 +169,11 @@ scouting_matches = {}
 # Loop through each team to choose which matches to scout
 for team, event in scouting_events.items():
     if event == None:
-        scouting_matches[team] = ["No event", []]
+        if not eventless:
+            scouting_matches[team] = ["No event", []]
         continue
     # Get a list of their matches from TBA
-    matches = tba_request("team/frc" + str(team) + "/event/" + event + "/matches")
+    matches = tba_request(f"team/frc{team}/event/{event}/matches")
     # Initialize match_list to hold a list of match keys
     match_list = []
     # If the match has videos, add it to match_list
@@ -200,9 +226,23 @@ for team, event in scouting_events.items():
         while(len(scouting_matches[team][1]) < 4 and len(team_matches["qm"]) > 0):
             scouting_matches[team][1].append(team_matches["qm"].pop()["key"].split("_")[1])
 
-output = open("output.txt","w")
-output.write("team;event;[matches]")
+output = open("output.csv", "w+", newline = "")
+fields = ["team", "event", "matches"]
+writer = csv.DictWriter(output, fields)
+writer.writeheader()
 for team in scouting_matches:
-    output.write(f"\n{team};{scouting_matches[team][0]};{scouting_matches[team][1]}")
+    writer.writerow({"team": team, "event": scouting_matches[team][0], "matches": str(scouting_matches[team][1])[1:-1].replace("'","")})
+
+"""for making text output"""
+# output = open("output.txt", "w+")
+# output.write("team;event;[matches]")
+# for team in scouting_matches:
+#     output.write(f"\n{team};{scouting_matches[team][0]};{str(scouting_matches[team][1])[1:-1].replace("'","")}")
+
+print("\nDone, check output.csv for your prescouting matches or copy the text below. Thank you, and have a nice day!\n")
 output.close()
-print("\nDone, check output.txt for your prescouting matches. Thank you, and have a nice day!", end = "")
+output = open("output.csv")
+print(output.read())
+output.close()
+session.close()
+exit()
